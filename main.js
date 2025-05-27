407cb95f32c886771b88db8e919cfae1cac29a5a
matheusgobetti <matheusgobetti12@gmail.com>
2025-05-27 18:21:09 -0300

    feat: list rendering

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const App = Vue.createApp({
   data() {
     return {
-      product: "Socks"
+      product: {
+        name: "Socks",
+        description: "Warm and cozy socks",
+      },
+      image: "./assets/images/socks_green.jpg",
+      url: "https://vuejs.org",
+      inventory: 8,
+      onSale: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
     }
   }
 })
