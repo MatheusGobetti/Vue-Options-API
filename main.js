1bb2135dfb10be5daa7bff5c8435dc2d402c7a1d
matheusgobetti <matheusgobetti12@gmail.com>
2025-05-27 19:23:22 -0300

    adicionando botão de remover itens do carrinho

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const App = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: {
         name: "Socks",
         description: "Warm and cozy socks",
@@ -10,6 +11,30 @@ const App = Vue.createApp({
       inventory: 8,
       onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [{
+        id: 2234,
+        color: "green",
+        image: "./assets/images/socks_green.jpg"
+      }, {
+        id: 2235,
+        color: "blue",
+        image: "./assets/images/socks_blue.jpg"
+      }]
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      }
     }
   }
 })
