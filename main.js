dd59a1176b6d4045db3586f2b72900cc2b2cc88d
matheusgobetti <matheusgobetti12@gmail.com>
2025-05-28 00:16:33 -0300

    feat: computed properties

--- main.js
+++ main.js
@@ -2,34 +2,40 @@ const App = Vue.createApp({
   data() {
     return {
       cart: 0,
+      brand: "Vue Mastery",
       product: {
         name: "Socks",
         description: "Warm and cozy socks",
       },
-      image: "./assets/images/socks_green.jpg",
+      selectedVariant: 0,
       url: "https://vuejs.org",
-      inventory: 8,
       onSale: true,
-      inStock: false,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [{
         id: 2234,
         color: "green",
-        image: "./assets/images/socks_green.jpg"
+        image: "./assets/images/socks_green.jpg",
+        quantity: 10
       }, {
         id: 2235,
         color: "blue",
-        image: "./assets/images/socks_blue.jpg"
+        image: "./assets/images/socks_blue.jpg",
+        quantity: 2
       }]
     }
   },
   methods: {
     addToCart() {
-      this.cart += 1;
+      if (this.cart < this.variants[this.selectedVariant].quantity) {
+        this.cart += 1;
+      }
     },
 
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      if (this.selectedVariant !== index) {
+        this.cart = 0;
+        this.selectedVariant = index;
+      }
     },
 
     removeFromCart() {
@@ -37,5 +43,19 @@ const App = Vue.createApp({
         this.cart -= 1;
       }
     }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product.name;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    inventory() {
+      return this.variants.reduce((sum, variant) => sum + variant.quantity, 0);
+    },
   }
 })
